6895f202bced66398d82bc97252fd7621f1b98d5
bmartel <brandonmartel@gmail.com>
2019-05-01 21:14:01 -0500

    Update setup.js

--- template/frameworks/ava/test/helpers/setup.js
+++ template/frameworks/ava/test/helpers/setup.js
@@ -1,8 +1,4 @@
 require('browser-env')()
 const hooks = require('require-extension-hooks')
-const Vue = require('vue')
 
-Vue.config.productionTip = false
-
-hooks('vue').plugin('vue').push();
-hooks(['vue', 'js']).plugin('babel').push();
+hooks(['js']).plugin('babel').push();
