ef49b825e79b9c09c65c278bb763c00cdad72e58
bmartel <brandonmartel@gmail.com>
2019-05-01 21:08:08 -0500

    Update jest.config.js

--- template/frameworks/jest/jest.config.js
+++ template/frameworks/jest/jest.config.js
@@ -1,17 +1,14 @@
 module.exports = {
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js'
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/src/$1'
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
+  moduleFileExtensions: ['js', 'json'],
   transform: {
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
+    '^.+\\.js$': 'babel-jest'
   },
   "collectCoverage": true,
   "collectCoverageFrom": [
-      "<rootDir>/components/**/*.vue",
-      "<rootDir>/pages/**/*.vue"
+      "<rootDir>/src/**/*.js"
   ]
 }
