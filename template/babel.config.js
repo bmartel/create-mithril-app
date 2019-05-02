4ce7a7bbf00431effd7c1190561b5efed19e4e5f
bmartel <brandonmartel@gmail.com>
2019-05-01 21:12:15 -0500

    Update babel.config.js

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,7 +1,8 @@
 module.exports = function(api) {
   api.cache.using(() => process.env.NODE_ENV === "production");
 
-  const presets = [["@babel/preset-env", { useBuiltIns: "entry" }]];
+  const isTest = process.env.NODE_ENV === 'test';
+  const presets = [["@babel/preset-env", isTest ?  {"targets": { "node": "current" }} : { useBuiltIns: "entry" }]];
   const plugins = ["@babel/plugin-syntax-dynamic-import", "@babel/plugin-proposal-object-rest-spread"];
 
   return {
