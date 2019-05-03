147de9f0c15cbda5600c8145241c0197b3bb5cdb
Brandon Martel <brandonmartel@gmail.com>
2019-05-02 20:55:50 -0500

    corejs usage

--- template/babel.config.js
+++ template/babel.config.js
@@ -2,7 +2,7 @@ module.exports = function(api) {
   api.cache.using(() => process.env.NODE_ENV === "production");
 
   const isTest = process.env.NODE_ENV === 'test';
-  const presets = [["@babel/preset-env", isTest ?  {"targets": { "node": "current" }} : { useBuiltIns: "entry" }]];
+  const presets = [["@babel/preset-env", isTest ?  {"targets": { "node": "current" }} : { useBuiltIns: "entry", "corejs": "3"}]];
   const plugins = ["@babel/plugin-syntax-dynamic-import", "@babel/plugin-proposal-object-rest-spread"];
 
   return {
