cae83cb0f24732ddffbd71d0def2a07d8a3d8ee8
Brandon Martel <brandonmartel@gmail.com>
2019-05-02 21:42:59 -0500

    seeing if replacing webpack devserver helps with heap memory overflows

--- template/mithril/config/webpack.dev.js
+++ template/mithril/config/webpack.dev.js
@@ -1,60 +1,36 @@
-const path = require("path");
-const fs = require("fs");
-const webpack = require("webpack");
+const path = require('path')
+const fs = require('fs')
+const webpack = require('webpack')
+const { WebpackPluginServe: Serve } = require('webpack-plugin-serve')
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const InterpolateHtmlPlugin = require("react-dev-utils/InterpolateHtmlPlugin");
-const ManifestPlugin = require("webpack-manifest-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const errorOverlayMiddleware = require("react-dev-utils/errorOverlayMiddleware");
-const evalSourceMapMiddleware = require("react-dev-utils/evalSourceMapMiddleware");
-const noopServiceWorkerMiddleware = require("react-dev-utils/noopServiceWorkerMiddleware");
-const ignoredFiles = require("react-dev-utils/ignoredFiles");
-const config = require("./config");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware')
+const evalSourceMapMiddleware = require('react-dev-utils/evalSourceMapMiddleware')
+const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware')
+const ignoredFiles = require('react-dev-utils/ignoredFiles')
+const config = require('./config')
 
-const protocol = process.env.HTTPS === "true" ? "https" : "http";
-const host = process.env.HOST || "0.0.0.0";
-const port = process.env.PORT || 9000;
-
-const devServer = {
-  disableHostCheck: process.env.DANGEROUSLY_DISABLE_HOST_CHECK === "true",
-  compress: true,
-  clientLogLevel: "none",
-  contentBase: config.paths.public,
-  watchContentBase: true,
-  hot: true,
-  publicPath: config.output.publicPath,
-  watchOptions: {
-    ignored: ignoredFiles(config.paths.app),
-  },
-  https: false,
-  host,
-  port,
-  overlay: false,
-  historyApiFallback: {
-    disableDotRule: true,
-  },
-  before(app, server) {
-    app.use(evalSourceMapMiddleware(server));
-    app.use(errorOverlayMiddleware());
-    app.use(noopServiceWorkerMiddleware());
-  },
-};
+const protocol = process.env.HTTPS === 'true' ? 'https' : 'http'
+const watch = process.env.NODE_ENV === 'development'
+const host = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT || 9000
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   devtool: config.devtool,
   entry: [config.paths.entry],
-  devServer,
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
       name: false,
     },
     runtimeChunk: true,
   },
   resolve: {
-    extensions: [".js", ".json"],
+    extensions: ['.js', '.json'],
     alias: config.paths.alias,
   },
   output: {
@@ -63,7 +39,7 @@ module.exports = {
     filename: config.output.filename,
     chunkFilename: config.output.chunkFilename,
     publicPath: config.output.publicPath,
-    devtoolModuleFilenameTemplate: info => path.resolve(info.absoluteResourcePath).replace(/\\/g, "/"),
+    devtoolModuleFilenameTemplate: info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
   },
   module: {
     strictExportPresence: true,
@@ -72,19 +48,19 @@ module.exports = {
         oneOf: [
           {
             test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
               limit: 10000,
-              name: "static/media/[name].[hash:6].[ext]",
+              name: 'static/media/[name].[hash:6].[ext]',
             },
           },
           {
             test: /\.mjs$/,
-            type: "javascript/auto",
+            type: 'javascript/auto',
           },
           {
             test: /\.js$/,
-            loader: "babel-loader",
+            loader: 'babel-loader',
             include: config.paths.js,
             options: {
               cacheDirectory: true,
@@ -93,17 +69,17 @@ module.exports = {
           },
           {
             test: /\.css$/,
-            use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
           },
           {
             test: /\.hbs$/,
-            loader: "handlebars-loader",
+            loader: 'handlebars-loader',
           },
           {
             exclude: [/\.(js|jsx)$/, /\.html$/, /\.hbs$/, /\.json$/],
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "static/media/[name].[hash:6].[ext]",
+              name: 'static/media/[name].[hash:6].[ext]',
             },
           },
         ],
@@ -112,7 +88,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env": {
+      'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
       },
     }),
@@ -123,14 +99,21 @@ module.exports = {
       fileName: config.output.manifest,
       publicPath: config.output.publicPath,
     }),
-    new webpack.HotModuleReplacementPlugin(),
+    new Serve({
+      host,
+      port,
+      hmr: true,
+      historyFallback: true,
+      static: [config.paths.public],
+    }),
   ],
+  watch,
   node: {
-    dgram: "empty",
-    fs: "empty",
-    net: "empty",
-    tls: "empty",
-    child_process: "empty",
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty',
     setImmediate: false,
   },
-};
+}
