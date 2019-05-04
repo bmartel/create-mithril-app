6db98ffa1bbeada0827f1625f3f47e097525f0ff
Brandon Martel <brandonmartel@gmail.com>
2019-05-03 21:38:30 -0500

    somewhat fix the hapijs template, more work will be needed on mitts to fully support it properly

--- template/frameworks/express/server/server.js
+++ template/frameworks/express/server/server.js
@@ -1,46 +1,71 @@
-require("mithril/test-utils/browserMock")(global)
+require('mithril/test-utils/browserMock')(global)
 
-import Mitts from "mitts"
-import { express as MittsExpress } from "mitts/loader"
-import bodyParser from "body-parser"
-import express from "express"
-import morgan from "morgan"
-import consola from 'consola';
-import path from "path"
-import cookieParser from "cookie-parser"
+import path from 'path'
+import consola from 'consola'
+import Mitts from 'mitts'
+import Loader from 'mitts/loader'
+import Hapi from '@hapi/hapi'
+import Inert from '@hapi/inert'
 
-import client from "../src/index"
+import client from '../src/index'
 
-async function start() {
-  const app = express()
-  const port = process.env.PORT || 3000
-  const host = process.env.HOST || 'localhost'
-  const buildDir = path.resolve(__dirname, "../build")
+const buildDir = path.resolve(__dirname, '../build')
 
-  const mitts = MittsExpress({
-    html: `${buildDir}/app.html`,
+const server = new Hapi.Server({
+  host: process.env.HOST || '127.0.0.1',
+  port: process.env.PORT || 3000,
+  routes: {
+    files: {
+      relativeTo: buildDir,
+    },
+  },
+})
+
+const mitts = new Loader(
+  (request, h) => ({
+    request: request.raw.req,
+    response: request.raw.res,
+    next: h.continue,
+  }),
+  {
+    html: `${buildDir}/index.html`,
     manifest: `${buildDir}/mitts.json`,
     createSession(cookies) {},
     createStore: client.store,
     routes: client.routes,
-  })
+  }
+)
 
-  app
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: false }))
-    .use(morgan("dev"))
-    .use(cookieParser())
-    .use(express.static(buildDir))
-    .use(mitts.middleware())
+async function start() {
+  try {
+    await server.register(Inert)
 
-  await Mitts.preloadAll()
+    server.route({
+      method: 'GET',
+      path: '/{param*}',
+      handler: {
+        directory: {
+          path: '.',
+          redirectToSlash: true,
+          index: true,
+        },
+      },
+    })
 
-  // Listen the server
-  app.listen(port, host)
+    server.ext('onPostHandler', mitts.middleware())
 
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
-  })
+    await Mitts.preloadAll()
+
+    await server.start()
+
+    consola.ready({
+      message: `Server running at: ${server.info.uri}`,
+      badge: true,
+    })
+  } catch (err) {
+    consola.error(err)
+    throw err
+  }
 }
+
 start()
--- template/frameworks/hapi/server/server.js
+++ template/frameworks/hapi/server/server.js
@@ -1,46 +1,55 @@
-require("mithril/test-utils/browserMock")(global);
+require('mithril/test-utils/browserMock')(global)
 
-import path from "path";
-import consola from 'consola';
-import Mitts from "mitts";
-import { Loader } from "mitts/loader";
-import Hapi from 'hapi'
+import path from 'path'
+import consola from 'consola'
+import Mitts from 'mitts'
+import { Loader } from 'mitts/loader'
+import Hapi from '@hapi/hapi'
+import Inert from '@hapi/inert'
 
-import client from "../src/index";
+import client from '../src/index'
 
-const buildDir = path.resolve(__dirname, "../build");
+const buildDir = path.resolve(__dirname, '../build')
 
 const server = new Hapi.Server({
   host: process.env.HOST || '127.0.0.1',
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  routes: {
+    files: {
+      relativeTo: buildDir,
+    },
+  },
 })
 
-server
-  .register({
-    plugin: async function(server, options) {
+async function start() {
+  await server.register(Inert);
 
-      // TODO: make this work with hapi
-      // app.use(mitts.middleware());
+  await server
+    .register({
+      plugin: async function(server, options) {
+        // TODO: make this work with hapi
+        app.use(mitts.middleware());
 
-      // const mitts = MittsExpress({
-      //   html: `${buildDir}/app.html`,
-      //   manifest: `${buildDir}/mitts.json`,
-      //   createSession(cookies) {},
-      //   createStore: client.store,
-      //   routes: client.routes,
-      // });
+        const mitts = MittsExpress({
+          html: `${buildDir}/app.html`,
+          manifest: `${buildDir}/mitts.json`,
+          createSession(cookies) {},
+          createStore: client.store,
+          routes: client.routes,
+        });
 
-      await Mitts.preloadAll()
-    }
-  })
-  .then(() => server.start())
-  .then(() =>
+        await Mitts.preloadAll()
+      },
+    })
+
+    await server.start()
+
     consola.ready({
       message: `Server running at: ${server.info.uri}`,
-      badge: true
+      badge: true,
     })
-  )
-  .catch(err => {
-    consola.error(err)
-    throw err
-  })
+    .catch(err => {
+      consola.error(err)
+      throw err
+    })
+}
