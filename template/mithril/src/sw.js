c1bf61bc4c2b7c4da8cf5d9a34b11732986d9416
Brandon Martel <brandonmartel@gmail.com>
2019-05-05 12:05:34 -0500

    add more strategies to base serviceworker

--- template/mithril/src/sw.js
+++ template/mithril/src/sw.js
@@ -1,8 +1,33 @@
+/* eslint-disable */
 workbox.core.skipWaiting()
 workbox.core.clientsClaim()
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+workbox.routing.registerRoute(/\/static\/.*\.(?:png|gif|jpg|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'static-images'
+  })
+);
+
+workbox.routing.registerRoute(/\/static\/.*\.(?:js|css)$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'static-resources'
+  })
+);
+
+workbox.routing.registerRoute(/.*(?:googleapis)\.com.*$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'googleapis'
+  })
+);
+
+workbox.routing.registerRoute(/.*(?:gstatic)\.com.*$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'gstatic'
+  })
+);
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL('/index.html'), {
   blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
